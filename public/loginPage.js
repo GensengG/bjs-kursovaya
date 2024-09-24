ba8706b7169149249ef3dcd16d586813081f60ff
GensengG <168531024+GensengG@users.noreply.github.com>
2024-09-24 21:11:23 +0300

    Update loginPage.js

--- public/loginPage.js
+++ public/loginPage.js
@@ -1,16 +1,24 @@
 'use strict'
 
 const userForm = new UserForm();
-userForm.registerFormCallback = function funcRegister(data){
-    ApiConnector.register(data, response => console.log(response));
+userForm.registerFormCallback = data => {
+    ApiConnector.register(data, response => {
+      let resultId = response.userId;
+      if(resultId > 0){
+          location.reload();
+      } else {
+      userForm.setLoginErrorMessage(response.error);
+      }
+    });
 };
 
-userForm.loginFormCallback = function funcLogin(data){
+userForm.loginFormCallback = data => {
   ApiConnector.login(data, response => {
-    console.log(response.userId);
-    if(response){
+    let resultId = response.userId;
+    if(resultId > 0){
         location.reload();
+    } else {
+        userForm.setLoginErrorMessage(response.error);
     }
-    userForm.setLoginErrorMessage();
   });
 }
